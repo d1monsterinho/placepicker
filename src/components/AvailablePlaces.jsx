e44431be3e256fe600acc0abc58092ea434181ff
Dmytro Yevich <dmytro.yevich@cengage.com>
2026-06-17 16:33:14 +0300

    Merge pull request #1: Add conditional loading

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,29 @@
 import Places from './Places.jsx';
+import {useEffect, useState} from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsLoading(true);
+
+      const response = await fetch('http://localhost:3000/places');
+      const body = await response.json();
+
+      setIsLoading(false);
+      setAvailablePlaces(body.places);
+    }
+
+    fetchPlaces();
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      isLoading={isLoading}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />

a97d925438ec555277aa80e2554e66c268355ed8
Dmytro Yevich <dmytro.yevich@cengage.com>
2026-06-17 17:28:34 +0300

    Implement error handling during all places fetching

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,23 +1,49 @@
 import Places from './Places.jsx';
+import ErrorDialog from './Error.jsx';
+
 import {useEffect, useState} from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   useEffect(() => {
+    const getPlacesUrl = 'http://localhost:3000/places';
+
     async function fetchPlaces() {
       setIsLoading(true);
 
-      const response = await fetch('http://localhost:3000/places');
-      const body = await response.json();
+      try {
+        const response = await fetch(getPlacesUrl);
 
-      setIsLoading(false);
-      setAvailablePlaces(body.places);
+        if (!response.ok) {
+          throw new Error()
+        }
+
+        const body = await response.json();
+        setAvailablePlaces(body.places);
+      } catch (error) {
+        console.log('in catch')
+        setError({
+          title: error.title || 'Data Fetching Error',
+          message: error.message || `Error Occurred Related to Data Fetching: ${getPlacesUrl}`,
+        })
+      } finally {
+        setIsLoading(false);
+      }
     }
 
     fetchPlaces();
   }, []);
+
+  function onErrorDialogConfirm() {
+    setError(undefined);
+  }
+
+  if (error) {
+    return <ErrorDialog title={error.title} message={error.message} onConfirm={onErrorDialogConfirm}/>
+  }
 
   return (
     <Places
